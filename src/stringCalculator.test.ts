9597accd08f423a07b077b7b9509e607b7dbab24
Amaresh Sahoo <41888623+AmareshSahoo@users.noreply.github.com>
2024-07-08 17:12:26 +0530

    Test cases written for handling string contains commas and newline

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -1,8 +1,20 @@
-import { StringCalculator } from './stringCalculator';
+import { StringCalculator } from "./stringCalculator";
 
-// It sould return 0 for an empty string
-describe('StringCalculator', () => {
-  test('should return 0 for an empty string', () => {
-    expect(StringCalculator.add('')).toBe(0);
+// It will test all the testcases for StringCalculator
+describe("StringCalculator", () => {
+  test("should return 0 for an empty string", () => {
+    expect(StringCalculator.add("")).toBe(0);
+  });
+
+  test("should return the number for a single number input", () => {
+    expect(StringCalculator.add("1")).toBe(1);
+  });
+
+  test("should return the sum for two numbers separated by a comma", () => {
+    expect(StringCalculator.add("1,2")).toBe(3);
+  });
+
+  test("should return the sum for multiple numbers separated by commas", () => {
+    expect(StringCalculator.add("1,2,3,4,5")).toBe(15);
   });
 });
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,6 +3,10 @@ export class StringCalculator {
     if (numbers === '' || !numbers) {
       return 0;
     }
-    return 0;
+
+    // Regex to match commas or newline characters.
+    const regex = /,|\n/;
+    const numberArray = numbers.split(regex);
+    return numberArray.reduce((sum, num) => sum + parseInt(num), 0);
   }
 }
